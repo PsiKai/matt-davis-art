87f0c7611c22e32f98cf0b1c761a83d206d6b53d
David <davidirvin47@gmail.com>
2021-10-15 19:49:26 -0600

    Get access token on every email

--- routes/contact.js
+++ routes/contact.js
@@ -1,25 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer')
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     console.log(req.body);
     const {address, subject, body, name} = req.body
-
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: 'dmatthew8282@gmail.com',
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: process.env.TOKEN,
-        }
-    });
-
     var emailFormat = 
     `<h2>From:</h2> 
     <p>${name} <br> ${address}</p>
@@ -28,23 +15,46 @@ router.post("/", (req, res) => {
         ${body}
     </p>`
 
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        'https://developers.google.com/oauthplayground'
+    )
+
+    oauth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN
+    })
+    
+    const accessToken = await oauth2Client.getAccessToken()
+    
+    let transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: accessToken
+        }
+    });
+
     var mailOptions = {
-        from: 'dmatthew8282@gmail.com',
-        to: 'dmatthew8282@gmail.com',
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
         subject: subject,
         html: emailFormat
     }
 
-    transporter.sendMail(mailOptions, (err, info) => {
+    transporter.sendMail(mailOptions, (err, response) => {
         if (err) {
+            console.log(err);
             res.json({msg: "There was an error sending the email. Please try again", color: "pink"})
         } else {
-            res.json({msg: "Your email was sent", color: "aliceblue"})
+            res.json({msg: "Your email was sent", color: "aliceblue"})  
+            transporter.close()
         }
     })
-  
-
-    
 })
 
 module.exports = router
