50cee23848461c3028ab3cafd377d5fbc0155875
David <davidirvin47@gmail.com>
2021-10-15 22:54:27 -0600

    Adjust email template styles

--- email-templates/order-req.js
+++ email-templates/order-req.js
@@ -3,31 +3,31 @@ const orderRequest = (artwork, shippingDetails, total) => {
 
     var requestEmail = 
     `<div style="background-color: #f9f9f9; color: #305973; font-family: sans-serif; padding: 1rem;">
-        <h1>You have a new order for Art!</h1>
-        <div style="max-width: 500px; padding: 0 2rem; border: 1px solid black; border-radius: 1rem; margin: 1rem;">    
-            <h2>Order Details:</h2>
-            <div style="padding: 1rem 2rem;">
-                <h3>Purchaser:</h3>
-                <div style="margin-left: 3rem;">
-                    <p>${name2}</p>
-                    <p>${email}</p>
+        <h2>You have a new order for Art!</h2>
+        <div style="padding: 0 1rem; width: max-content; border: 1px solid black; border-radius: 1rem;">    
+            <h2>Order Details</h2>
+            <div>
+                <div style="margin-bottom: 1.5rem">
+                    <h3>Purchaser:</h3>
+                    <p style="margin: 0">${name2}</p>
+                    <p style="margin: 0">${email}</p>
                 </div>
-                <h3>Shipping Address:</h3>
-                <div style="margin-left: 3rem;">
-                    <p>${name}</p>
-                    <p style="margin-bottom: 0.5rem;">${add1}</p>
-                    <p style="margin: 0.5rem 0;">${add2}</p>
+                <div style="margin-bottom: 1.5rem">
+                    <h3>Shipping Address:</h3>
+                    <p style="margin: 0;">${add1}</p>
+                    <p style="margin: 0;">${add2}</p>
+                    <p style="margin: 0;">${name}</p>
                     <span>${city},</span>
                     <span>${state}</span>
                     <span>${zip}</span>
                 </div>
-                <h3>Items Purchased:</h3>
-                <div style="text-align: center">
+                <div style="margin-bottom: 1.5rem">
+                    <h3>Items Purchased:</h3>
                     ${artwork}
                 </div>
                 <h3>Amount charged: $${total}</h3>
             </div>
-        </div>
+        </div style="margin-bottom: 1.5rem">
     </div>`
 
     return requestEmail
