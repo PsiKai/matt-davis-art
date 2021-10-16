5c5e2079031b3a73f7eaf0cd5b0806787ef3bb78
David <davidirvin47@gmail.com>
2021-10-15 23:02:50 -0600

    Finalize nodemailer configuration

--- routes/cart.js
+++ routes/cart.js
@@ -19,9 +19,8 @@ router.post("/checkout", (req, res) => {
 })
 
 
-router.post("/purchase", (req, res) => {
-    const {ship, ship: email, name, items, total} = req.body
-
+router.post("/purchase", async (req, res) => {
+    const {ship, ship: { email, name }, items, total} = req.body
     var art = artwork(items)
 
     const oauth2Client = new OAuth2(
@@ -48,43 +47,18 @@ router.post("/purchase", (req, res) => {
         }
     });
 
-    // let transporter = nodemailer.createTransport({
-    //     service: "Gmail",
-    //     port: 465,
-    //     secure: true,
-    //     auth: {
-    //         type: 'OAuth2',
-    //         clientId: process.env.CLIENT_ID,
-    //         clientSecret: process.env.CLIENT_SECRET,
-    //     }
-    // });
-
-    // transporter.on("token", token => {
-    //     console.log(token.user, token.accessToken, token.expires);
-    // })
-
     var mailOptions = {
         from: process.env.EMAIL,
         to: process.env.EMAIL,
         subject: "New order from " + name,
-        html: orderRequest(art, ship, total),
-        // auth: {
-        //     user: 'dmatthew8282@gmail.com',
-        //     refreshToken: process.env.REFRESH_TOKEN,
-        //     accessToken: process.env.TOKEN,
-        // }
+        html: orderRequest(art, ship, total)
     }
 
     var mailOptions2 = {
         from: process.env.EMAIL,
         to: email,
         subject: "Your purchase from Artist Matt Davis",
-        html: orderConfirmation(art, ship, total),
-        // auth: {
-        //     user: 'dmatthew8282@gmail.com',
-        //     refreshToken: process.env.REFRESH_TOKEN,
-        //     accessToken: process.env.TOKEN,
-        // }
+        html: orderConfirmation(art, ship, total)
     }
 
     var orderReq = transporter.sendMail(mailOptions)
@@ -92,7 +66,7 @@ router.post("/purchase", (req, res) => {
 
     Promise.all([orderReq, orderConfirm])
         .then(([result1, result2]) => {
-            console.log("Emails sent", result1, result2);
+            console.log("Purchase emails sent");
             res.json({
                 code: 200,
                 heading: "Thank you for your purchase!", 
@@ -106,6 +80,9 @@ router.post("/purchase", (req, res) => {
                 heading: "I'm sorry, the order was not received",
                 msg: "Please check with paypal or your bank that the payment was made, then send me an email and I will make sure you get your order!"
             })
+        })
+        .finally(() => {
+            transporter.close()
         })
 })
 
