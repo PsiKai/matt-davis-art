71e0fd948ccce254054878b5175a8086edf28c78
David <davidirvin47@gmail.com>
2021-10-23 10:00:35 -0600

    Improve image loading experience
    - Add function to increment img loading
    - Display spinner until all img srcs load
    - Set loaded state when all done and fade in content

--- client/src/pages/Prints.js
+++ client/src/pages/Prints.js
@@ -14,12 +14,16 @@ const Prints = () => {
     const [modalOpen, setModalOpen] = useState(false)
     const [quantity, setQuantity] = useState(1)
     const [mount, setMount] = useState(false);
+    const [counted, setCount] = useState(0)
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         !prints && getArt();
         setMount(true)
         //eslint-disable-next-line
     }, [])
+
+    const fillerArray = [1,2,3]
 
     const openModal = (item) => {
         if (item[0].classList.contains("print-item")) {
@@ -52,8 +56,6 @@ const Prints = () => {
     }
 
     const addToCart = () => {
-        // const info = e.target.parentNode.children[3].children
-        // console.log(img.name);
         const item = {
             quantity: quantity,
             id: img.name
@@ -63,6 +65,13 @@ const Prints = () => {
         setModalOpen(false)
         setImg({})
         setQuantity(1)
+    }
+
+    const incrementLoaded = () => {
+        setCount(counted + 1)
+        if (counted + 1 === prints.length) {
+            setLoaded(true)
+        }
     }
 
     return (
@@ -82,29 +91,28 @@ const Prints = () => {
                 
             </div>
             <div className="prints-flexbox">
-            {prints ? 
-                prints.map((print, index) => {
-                    if (print.original === false) {
-                    return <Print
-                        key={index}
-                        id={index}
-                        src={print.img}
-                        price={print.price}
-                        title={print.title}
-                        sku={print._id}
-                        sold={print.soldOut}
-                        open={openModal}
-                        size={print.dimensions}
-                        /> 
-                    } else {
-                        return null
-                    }
-                })
-                : 
-                <div className="progress">
-                    <CircularProgress color="inherit"/>
-                </div>
-            }
+                {prints &&
+                    prints.map((print, index) => {
+                        if (print.original === false) {
+                        return <Print
+                            key={index}
+                            id={index}
+                            src={print.img}
+                            price={print.price}
+                            title={print.title}
+                            sku={print._id}
+                            sold={print.soldOut}
+                            open={openModal}
+                            size={print.dimensions}
+                            incrementLoaded={incrementLoaded}
+                            loaded={loaded}
+                            /> 
+                        } else {
+                            return null
+                        }
+                    })
+                }
+                <CircularProgress style={{opacity: loaded ? "0" : "1"}}/>
             </div>
             <div className="print-orders">
                 <h2>Original Artwork</h2>
@@ -112,25 +120,28 @@ const Prints = () => {
                 <div className="brand-backdrop"></div>
             </div>
             <div className="prints-flexbox">
-            {prints && 
-                prints.map((print, index) => {
-                    if (print.original === true) {
-                    return <Print
-                        key={index}
-                        id={index}
-                        src={print.img}
-                        price={print.price}
-                        title={print.title}
-                        sku={print._id}
-                        sold={print.soldOut}
-                        open={openModal}
-                        size={print.dimensions}
-                        /> 
-                    } else {
-                        return null
-                    }
-                })
-            }
+                {prints && 
+                    prints.map((print, index) => {
+                        if (print.original === true) {
+                        return <Print
+                            key={index}
+                            id={index}
+                            src={print.img}
+                            price={print.price}
+                            title={print.title}
+                            sku={print._id}
+                            sold={print.soldOut}
+                            open={openModal}
+                            size={print.dimensions}
+                            incrementLoaded={incrementLoaded}
+                            loaded={loaded}
+                            /> 
+                        } else {
+                            return null
+                        }
+                    })
+                }
+                <CircularProgress style={{opacity: loaded ? "0" : "1"}}/>
             </div>
             <TransitionGroup>
             {modalOpen && 
