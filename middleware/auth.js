5d3c28ec377db51c582446f4ffe356966f1e521e
David <davidirvin47@gmail.com>
2021-10-30 14:34:05 -0600

    Send index.html when user not authenticated in prod

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,6 +14,9 @@ module.exports = function (req, res, next) {
         req.user = decoded.user;
         next();
     } catch (err) {
+        if(process.env.NODE_ENV === 'production') {
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+        } 
         res.status(401).json({msg: "token is not valid"})
     }
 }
