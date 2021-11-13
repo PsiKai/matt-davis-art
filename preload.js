cf65ef18511fede6189715bba93dbb9755f40f34
David <davidirvin47@gmail.com>
2021-11-13 13:54:10 -0700

    Add preload script for js and css files

---
+++ preload.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const pathToEntry = "./client/build/index.html";
+const bundlesRegExp = /\/static\/\w+\/\w+.[a-z0-9]+.[a-z0-9]+.\w{2,3}/g;
+const splitBy = "</title>";
+
+const builtHTMLContent = fs.readFileSync(pathToEntry).toString();
+const links = builtHTMLContent.match(bundlesRegExp);
+const parts = builtHTMLContent.split(splitBy);
+
+let headContent = [parts[0], splitBy];
+
+links.forEach(link => {
+  let fileType = "script";
+
+  if (/\.css$/.test(link)) {
+    fileType = "style";
+  }
+//   fileWithPreload = [
+//     ...fileWithPreload,
+//     `<link rel="preload" href=".${link}" as="${fileType}">`
+//   ];
+headContent.push(`<link rel="preload" href=".${link}" as="${fileType}">`)
+});
+
+// fileWithPreload = [...fileWithPreload, parts[1]];
+headContent.push(parts[1]);
+
+fs.writeFileSync(pathToEntry, headContent.join(""));
