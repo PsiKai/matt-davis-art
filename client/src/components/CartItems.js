c9ad0bd125f89952d6abf34f323a6dc20ffa2d9f
David <davidirvin47@gmail.com>
2021-11-14 16:54:45 -0700

    Move paypal and cart modal to modals folder

--- client/src/components/CartItems.js
+++ client/src/components/CartItems.js
@@ -1,6 +1,6 @@
 import React, {Fragment, useContext, useEffect, useState} from 'react'
 import CartItem from './CartItem'
-import Modal from "../components/Modal"
+import Modal from "./modals/Modal"
 import ShippingForm from "./ShippingForm.js"
 import AppContext from "../context/AppContext";
 import AlertContext from "../context/alertContext";
