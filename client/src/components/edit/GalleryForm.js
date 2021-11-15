d9180b3c870f314faa373dbb5c48dd411b95737e
David <davidirvin47@gmail.com>
2021-11-14 18:23:34 -0700

    Extract upload gallery form to separate component

---
+++ client/src/components/edit/GalleryForm.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const GalleryForm = ({form: { title, medium, description}, formUpdate, imgUpdate, upload, file }) => {
+    return (
+        <form onSubmit={upload}>
+                <label className={file ? "file-input__label small-label" : "file-input__label"}>
+                    <span>Choose A File</span>
+                    <span className="file-input__name">{file.name}</span>
+                    <input 
+                        id="gallery-image" 
+                        type="file" 
+                        onChange={imgUpdate}
+                        required />
+                </label>
+                <div className="input__wrapper">
+                <label htmlFor="gallery-title">Title</label>
+                <input 
+                    id="gallery-title" 
+                    type="text" 
+                    name="title" 
+                    onChange={formUpdate} 
+                    value={title}
+                    required>
+                </input>
+                </div>
+
+                <div className="input__wrapper">
+                <label htmlFor="gallery-medium">Medium</label>
+                <input 
+                    id="gallery-medium" 
+                    type="text" 
+                    name="medium" 
+                    onChange={formUpdate} 
+                    value={medium}
+                    required>
+                </input>
+                </div>
+
+                <div className="input__wrapper">
+                <label htmlFor="title">Description</label>
+                <textarea 
+                    id="description" 
+                    type="text" 
+                    name="description" 
+                    rows="7"
+                    onChange={formUpdate}
+                    value={description}
+                    required>
+                </textarea>
+                </div>
+            
+                <button data-text="Submit" type="submit">Submit</button>
+                
+            </form>
+    )
+}
+
+export default GalleryForm
--- client/src/components/edit/UploadGallery.js
+++ client/src/components/edit/UploadGallery.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react'
 import AppContext from "../../context/AppContext"
 import AlertContext from "../../context/alertContext"
+import GalleryForm from './GalleryForm'
 import axios from 'axios'
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
@@ -82,57 +83,13 @@ const UploadGallery = () => {
         <div className="upload-gallery">
             <h2>Add Artwork to Gallery</h2>
             <div className="upload-form">
-            
-            <form onSubmit={upload}>
-                <label className={file ? "file-input__label small-label" : "file-input__label"}>
-                    <span>Choose A File</span>
-                    <span className="file-input__name">{file.name}</span>
-                    <input 
-                        id="gallery-image" 
-                        type="file" 
-                        onChange={imgUpdate}
-                        required />
-                </label>
-                <div className="input__wrapper">
-                <label htmlFor="gallery-title">Title</label>
-                <input 
-                    id="gallery-title" 
-                    type="text" 
-                    name="title" 
-                    onChange={formUpdate} 
-                    value={title}
-                    required>
-                </input>
-                </div>
-
-                <div className="input__wrapper">
-                <label htmlFor="gallery-medium">Medium</label>
-                <input 
-                    id="gallery-medium" 
-                    type="text" 
-                    name="medium" 
-                    onChange={formUpdate} 
-                    value={medium}
-                    required>
-                </input>
-                </div>
-
-                <div className="input__wrapper">
-                <label htmlFor="title">Description</label>
-                <textarea 
-                    id="description" 
-                    type="text" 
-                    name="description" 
-                    rows="7"
-                    onChange={formUpdate}
-                    value={description}
-                    required>
-                </textarea>
-                </div>
-            
-                <button data-text="Submit" type="submit">Submit</button>
-                
-            </form>
+            <GalleryForm 
+                form={form} 
+                imgUpdate={imgUpdate} 
+                formUpdate={formUpdate} 
+                upload={upload} 
+                file={file}
+            />
             <TransitionGroup className="img-preview" style={preview !== "" ? {} : {display: "none" }}>
                 <CSSTransition
                     key={file.size}
