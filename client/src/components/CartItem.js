ec358c6d04956f2249a3522683c0a534b547ab0c
david irvin <davidirvin47@gmail.com>
2022-02-21 21:14:23 -0700

    Refactor cartItem component
     - Clean up transitions
     - Remove redundancies in edit
     - Flexbox all the things
     - Replace font awesome with material ui icons

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -2,6 +2,9 @@ import React, { useState, useContext, useEffect} from 'react'
 import AppContext from '../context/AppContext'
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import ImgModal from './modals/ImgModal';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@material-ui/icons/RemoveCircleOutlineOutlined';
+import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
 
 const CartItem = ({quantity, title, src, id, original, size, price}) => {
     const appContext = useContext(AppContext)
@@ -90,77 +93,74 @@ const CartItem = ({quantity, title, src, id, original, size, price}) => {
                         onClick={fullSize} 
                     />
                 </div>
-                {original ? 
-                    <div className="cart-item--info__wrapper">
-                        <h2>{title}</h2>
-                        <h4>Original Artwork</h4>
-                        <h5>{size.width}" x {size.height}"</h5>
-                        <h4>${price}</h4>
-                        <h5>The one and only!</h5>
-                        <button onClick={removeArt}><i className="far fa-minus-square fa-lg"></i></button>
-                    </div> :
-
-                    
                 <div className="cart-item--info__wrapper">
                     <h2>{title}</h2>
-                    <h4>High Quality Print</h4>
-                    <h5>11 x 17</h5>
-                    <h4>${price} each</h4>
-                    <p>Quantity: </p>
-                    <TransitionGroup className="cart-item--quantity__wrapper">
-                    
-                    {edit === true ? 
-                        <CSSTransition
-                            key={1}
-                            classNames="switch"
-                            timeout={100}
-                        >
-                        <form className="cart-item--quantity" onSubmit={makeChanges}>
-                            <input 
-                                id="cart-quantity"
-                                name="quantity"
-                                type="number"
-                                inputMode="numeric"
-                                min="0"
-                                value={quan}
-                                onChange={updateQuantity}
-                            />
-                        <button><i className="far fa-check-square fa-lg"></i></button>
-                        </form> 
-                        </CSSTransition>:
-
-                        <CSSTransition
-                            key={2}
-                            classNames="switch"
-                            timeout={100}
-                        >
-                        <div className="cart-item--quantity">
-                            <span>{quan}</span>
-                            <button onClick={adjustQuan}>
-                                <i className="far fa-edit fa-lg"></i>
-                            </button>
+                    <div className="cart-item--info">
+                    <h4>{original ? "Original Artwork" : "High Quality Print"}</h4>
+                    <h5>{size.width} x {size.height}</h5>
+                    <h4>${price} {!original && "each"}</h4>
+                    </div>
+                    {original ?
+                        <div className='cart-item--quantity'>
+                            <h5>The one and only!</h5>
+                            <button onClick={removeArt}><RemoveCircleOutlineOutlinedIcon/></button>
                         </div>
-                        </CSSTransition>
-                    }
-                    </TransitionGroup>
+                    :
+                    <>
+                    <CSSTransition
+                        in={edit}
+                        classNames="switch"
+                        timeout={100}
+                        unmountOnExit
+                    >
+                        <form className="cart-item--quantity" onSubmit={makeChanges}>
+                            <div className='input__wrapper'>
+                                <label>Quantity:</label>
+                                <input
+                                    id="cart-quantity"
+                                    name="quantity"
+                                    type="number"
+                                    inputMode="numeric"
+                                    min="0"
+                                    value={quan}
+                                    onChange={updateQuantity}
+                                />
+                            </div>
+                            <div className='button-group'>
+                                <button onClick={removeArt}><RemoveCircleOutlineOutlinedIcon/></button>
+                                <button type="submit" onClick={adjustQuan}><CheckBoxOutlinedIcon/></button>
+                            </div>
+                        </form> 
+                    </CSSTransition>
+
+                    <CSSTransition
+                        in={!edit}
+                        classNames="switch"
+                        timeout={100}
+                        unmountOnExit
+                    >
+                        <div className="cart-item--quantity">
+                            <span>Quantity: {quan}</span>
+                            <div className='button-group'>
+                                <button onClick={removeArt}><RemoveCircleOutlineOutlinedIcon/></button>
+                                <button onClick={adjustQuan}><EditOutlinedIcon/></button>
+                            </div>
+                        </div>
+                    </CSSTransition>
+                    </>}
                 </div>
-                }
-                <TransitionGroup>
-                    {imgModal.in &&
-                        
-                        <CSSTransition
-                            key={title}
-                            classNames="fadein" 
-                            timeout={200}
-                        >
-                            <ImgModal 
-                                close={close} 
-                                img={imgModal.src} 
-                                title={imgModal.title}
-                            />
-                        </CSSTransition>
-                    }
-                </TransitionGroup>
+                <CSSTransition
+                    in={imgModal.in}
+                    classNames="fadein"
+                    timeout={200}
+                    unmountOnExit
+                >
+                    <ImgModal
+                        close={close}
+                        img={imgModal.src}
+                        title={imgModal.title}
+                    />
+                </CSSTransition>
             </div>     
         </CSSTransition>
     )
