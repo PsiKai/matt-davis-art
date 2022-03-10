9673fc11276b950ae8721b82475778f67073d336
david irvin <davidirvin47@gmail.com>
2022-03-09 21:18:18 -0700

    Fix lazy load image implementation

--- client/src/components/Piece.js
+++ client/src/components/Piece.js
@@ -1,43 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import {CSSTransition} from 'react-transition-group'
+import React from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import { CircularProgress } from '@material-ui/core'
 
-const Piece = ({src, alt, title, medium, description, id}) => {
-    const [fade, setFade] = useState(false)
-
-    useEffect(() => {
-        setFade(true)
-    }, [])
-
-
-    // var bytes = Buffer.from(src.data)
+const LoadingComponent = () => {
     return (
-        <CSSTransition 
-            in={fade} 
-            classNames="fadein" 
-            timeout={400}
-            unmountOnExit={true}
-        >
-        <div style={{transitionDelay: `${id * 50}ms`}}>
-            <hr className="art-division" />
-            <div className="gallery-piece" id={title.replace(/ /g, "-")}>    
-                <LazyLoadImage 
-                    src={src}
-                    // {`data:${src.contentType};base64, ${bytes.toString('base64')}`} 
-                    alt={alt}
-                    effect="opacity"
-                    placeholder={<CircularProgress/>}
-                />
-                <div className="plaque">
-                    <h3><em>{title}</em></h3>
-                    <p className="plaque-medium">{medium}</p>
-                    <p className="plaque-description">{description}</p>
-                </div>
-            </div> 
+        <div className='loading'>
+            <CircularProgress/>
         </div>
-        </CSSTransition>
-        
+    )
+}
+
+const Piece = ({ src, alt, title, medium, description }) => {
+    return (
+        <>
+        <hr className="art-division" />
+        <div className="gallery-piece">
+            <LazyLoadImage
+                src={src}
+                alt={alt}
+                effect="opacity"
+                useIntersectionObserver={true}
+                placeholder={<LoadingComponent/>}
+            />
+            <div className="plaque">
+                <h3><em>{title}</em></h3>
+                <p className="plaque-medium">{medium}</p>
+                <p className="plaque-description">{description}</p>
+            </div>
+        </div>
+        </>
     )
 }
 
