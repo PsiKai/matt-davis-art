67569dd33b8bb6178599adf3ebffac612c7ce0e8
david irvin <davidirvin47@gmail.com>
2022-03-17 19:29:55 -0600

    Add styles for fallback image components

---
+++ client/src/components/layout/PlaceholderImg.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PanoramaOutlinedIcon from '@material-ui/icons/PanoramaOutlined';
+
+const PlaceholderImg = () => {
+    const dots = [1, 2, 3, 4, 5]
+
+    return (
+        <div className='loading'>
+            <PanoramaOutlinedIcon />
+            <div className='loading-dots'>
+                {dots.map((j, i) => (
+                    <span className='dot' style={{ animationDelay: `${i * 80}ms`, ['--jump']: `-${Math.pow(2, 1 / Number(1 + j)) * 100}%` }}></span>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default PlaceholderImg
