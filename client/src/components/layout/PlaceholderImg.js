9ddb763ef2e6a042bc113fc6d0b90f5ab197e00a
david irvin <davidirvin47@gmail.com>
2022-03-18 22:40:05 -0600

    Replace slow math calculation with hardcoded values

--- client/src/components/layout/PlaceholderImg.js
+++ client/src/components/layout/PlaceholderImg.js
@@ -2,25 +2,50 @@ import React from 'react'
 import PanoramaOutlinedIcon from '@material-ui/icons/PanoramaOutlined';
 
 const PlaceholderImg = () => {
-    const dots = [1, 2, 3, 4, 5]
-
     return (
         <div className='loading'>
             <PanoramaOutlinedIcon />
             <div className='loading-dots'>
-                {dots.map((j, i) => (
-                    <span
-                        className='dot'
-                        key={j}
-                        style={{
-                            animationDelay: `${i * 80}ms`,
-                            '--jump': `-${Math.pow(2, 1 / Number(1 + j)) * 100}%`
-                        }}
-                    ></span>
-                ))}
+                <span
+                    className='dot'
+                    style={{
+                        animationDelay: "0ms",
+                        '--jump': "-141%"
+                    }}
+                ></span>
+                <span
+                    className='dot'
+                    style={{
+                        animationDelay: "160ms",
+                        '--jump': "-125%"
+                    }}
+                ></span>
+                <span
+                    className='dot'
+                    style={{
+                        animationDelay: "240ms",
+                        '--jump': "-118%"
+                    }}
+                ></span>
+                <span
+                    className='dot'
+                    style={{
+                        animationDelay: "320ms",
+                        '--jump': "-114%"
+                    }}
+                ></span>
+                <span
+                    className='dot'
+                    style={{
+                        animationDelay: "400ms",
+                        '--jump': "-112%"
+                    }}
+                ></span>
             </div>
         </div>
     )
 }
+
+// `-${Math.pow(2, 1 / Number(1 + j)) * 100}%`
 
 export default PlaceholderImg
