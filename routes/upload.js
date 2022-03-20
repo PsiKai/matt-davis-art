75cb7489ef45932fb247da5c63514e7bc49b59ee
david irvin <davidirvin47@gmail.com>
2022-03-19 20:59:23 -0600

    Convert image to webp and combine prints and gallery routes

--- routes/upload.js
+++ routes/upload.js
@@ -2,40 +2,43 @@ const express = require("express")
 const router = express.Router();
 const fs = require("fs")
 const path = require("path");
-const {Storage} = require("@google-cloud/storage")
+const sharp = require("sharp")
+
 let auth 
 if(process.env.NODE_ENV !== 'production') {
-    auth = "../config/matt-gcp-oauth.json"
+    auth = process.env.GOOGLE_APPLICATION_CREDENTIALS
 } else {
     auth = "google-credentials.json"
 }
 
+const {Storage} = require("@google-cloud/storage")
 const storage = new Storage({keyFileName: auth});
 
-const printBucket = "matt-d-prints"
-const galleryBucket = "matt-d-gallery"
-
-var galleryModel = require("../models/gallery")
-var printModel = require("../models/prints")
-
-router.post("/gallery", async (req, res) => {
+router.post("/:destination", async (req, res) => {
     if (!req.files) res.status(400).json({msg: "No file was received"})
 
-    const { title, description, medium, position } = req.body
-    const { file: { name }, file } = req.files
-    const dirFileName = encodeURIComponent(name)
-    const type = name.split(/\.(?=[^\.]+$)/)[1]
-    const newName = title + Date.now() + "." + type
-    const cloudName = encodeURIComponent(newName)
-    const img = encodeURI(`https://storage.googleapis.com/${galleryBucket}/${cloudName}`)
-    var imgObj = { title, description, medium, type, img, position }
+    const { destination } = req.params
+    const { title } = req.body
+    const { file: { data } } = req.files
+
+    const bucket = `matt-d-${destination}`
+    const model = require(`../models/${destination}`)
+
+    const newName = title + Date.now() + ".webp"
+    const dirFileName = encodeURIComponent(newName)
     const localPath = path.join(__dirname, '/uploads/', dirFileName)
+    const img = encodeURI(`https://storage.googleapis.com/${bucket}/${dirFileName}`)
+
+    var imgObj = buildImgObject(req.body, destination)
+    imgObj = {...imgObj, img}
+
+    const resDestination = destination === "gallery" ? destination : "store"
 
     try {
-        await file.mv(localPath)
-        await storage.bucket(galleryBucket).upload(localPath, {destination: cloudName})
-        await galleryModel.create(imgObj)
-        res.json({msg: `${title} was uploaded to the gallery!`})
+        await sharp(data).webp().toFile(localPath)
+        await storage.bucket(bucket).upload(localPath, {destination: dirFileName})
+        await model.create(imgObj)
+        res.json({msg: `${title} was uploaded to the ${resDestination}!`})
     } catch (error) {
         console.log(error)
         res.json({msg: `There was an error uploading ${title}`})
@@ -45,32 +48,14 @@ router.post("/gallery", async (req, res) => {
     }
 })
 
-
-router.post("/prints", async (req, res) => {
-    if (!req.files) res.status(400).json({msg: "No file was received"})
-
-    const { title, original, price, dimensions, position } = req.body
-    const { file: { name }, file } = req.files
-    const dirFileName = encodeURIComponent(name)
-    const type = name.split(/\.(?=[^\.]+$)/)[1]
-    const newName = title + Date.now() + "." + type
-    const cloudName = encodeURIComponent(newName)
-    const img = encodeURI(`https://storage.googleapis.com/${printBucket}/${cloudName}`)
-    var imgObj = { title, original, price, dimensions, type, soldOut: false, img, position }
-    const localPath = path.join(__dirname, '/uploads/', dirFileName)
-
-    try {
-        await file.mv(localPath)
-        await storage.bucket(printBucket).upload(localPath, {destination: cloudName})
-        await printModel.create(imgObj)
-        res.json({msg: `${title} was uploaded to the store!`})
-    } catch (error) {
-        console.log(error)
-        res.json({msg: `There was an error uploading ${title}`})
-    } finally {
-        const localFile = fs.existsSync(localPath)
-        if (localFile) fs.unlinkSync(localPath)
+const buildImgObject = (reqBody, destination) => {
+    if (destination === "gallery") {
+        const { title, description, medium, position } = reqBody
+        return { title, description, medium, position }
+    } else {
+        const { title, original, price, dimensions, position } = reqBody
+        return { title, original, price, dimensions, position }
     }
-})
+}
 
 module.exports = router;
