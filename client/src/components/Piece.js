da5677f7a5e3e895e069121a2c12281f4b4bb465
david irvin <davidirvin47@gmail.com>
2022-03-19 19:14:43 -0600

    Remove old loading component

--- client/src/components/Piece.js
+++ client/src/components/Piece.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import 'react-lazy-load-image-component/src/effects/opacity.css';
-import { CircularProgress } from '@material-ui/core'
-
-const LoadingComponent = () => {
-    return (
-        <div className='loading'>
-            <CircularProgress/>
-        </div>
-    )
-}
+import ImageFallbacks from './layout/ImageFallbacks';
 
 const Piece = ({ src, alt, title, medium, description }) => {
+    const [loaded, setLoaded] = useState(false)
+    const [brokenLink, setBrokenLink] = useState(false)
+
     return (
         <>
         <hr className="art-division" />
         <div className="gallery-piece">
+            <ImageFallbacks title={title} loaded={loaded} brokenLink={brokenLink} />
             <LazyLoadImage
                 src={src}
                 alt={alt}
                 effect="opacity"
                 useIntersectionObserver={true}
-                placeholder={<LoadingComponent/>}
+                afterLoad={() => setLoaded(true)}
+                onError={() => {
+                    setLoaded(true)
+                    setBrokenLink(true)
+                }}
             />
             <div className="plaque">
                 <h3><em>{title}</em></h3>
