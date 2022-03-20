d3739902d03e3d1af9c73081230874a112a854bf
david irvin <davidirvin47@gmail.com>
2022-03-19 19:14:09 -0600

    Cleanup fallback images component

--- client/src/components/layout/ImageFallbacks.js
+++ client/src/components/layout/ImageFallbacks.js
@@ -5,27 +5,27 @@ import BrokenImgFallback from './BrokenImgFallback';
 
 const ImageFallbacks = (props) => {
     const { loaded, brokenLink, title } = props
-    console.log(title)
-  return (
-    <>
-        <CSSTransition
-            in={!loaded}
-            timeout={400}
-            classNames="crossfade"
-            unmountOnExit
-        >
-            <PlaceholderImg />
-        </CSSTransition>
-        <CSSTransition
-            in={brokenLink}
-            timeout={400}
-            classNames="crossfade"
-            unmountOnExit
-        >
-            <BrokenImgFallback imgTitle={ title }/>
-        </CSSTransition>
-    </>
-  )
+    
+    return (
+        <>
+            <CSSTransition
+                in={!loaded}
+                timeout={400}
+                classNames="crossfade"
+                unmountOnExit
+            >
+                <PlaceholderImg />
+            </CSSTransition>
+            <CSSTransition
+                in={brokenLink}
+                timeout={400}
+                classNames="crossfade"
+                unmountOnExit
+            >
+                <BrokenImgFallback imgTitle={ title }/>
+            </CSSTransition>
+        </>
+    )
 }
 
 export default ImageFallbacks
