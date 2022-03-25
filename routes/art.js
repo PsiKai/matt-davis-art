8b535ddd5e0430476150bef330b1244e22e1444a
david irvin <davidirvin47@gmail.com>
2022-03-24 22:01:17 -0600

    Refactor get art routes to use async/await and return non deleted records

--- routes/art.js
+++ routes/art.js
@@ -7,44 +7,34 @@ var printModel = require("../models/prints")
 
 let art = {}
 
-router.get("/", (req, res) => {
-    // console.log(art);
+router.get("/", async (req, res) => {
     if (Object.keys(art).length > 0) {
         res.json(art)
-        console.log("already have art");
+        console.log("Already have art");
     } else {
-        console.log("getting art");
-        const promise1 = galleryModel.find({}).exec()
-        const promise2 = printModel.find({}).exec()
-        Promise.all([promise1, promise2])
-        .then(([result1, result2]) => {
-            console.log("got art");
-            art = {gallery: [...result1], prints: [...result2]}
-            res.json(art)
-        })
-        .catch(err => {
-            return res.status(400).json({
-                error: "Couldn't get art"
-            })
-        })   
+        try {
+            const prints = await printModel.find({ deletedAt: null })
+            const gallery = await galleryModel.find({ deletedAt: null })
+            console.log("Got art");
+            res.json({ gallery, prints })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: "Couldn't get art "})
+        }
     }
 })
 
-router.get("/refresh", (req, res) => {
-    console.log("refreshing");
-    const promise1 = galleryModel.find({}).exec()
-    const promise2 = printModel.find({}).exec()
-    Promise.all([promise1, promise2])
-        .then(([result1, result2]) => {
-            // console.log(result1, result2);
-            art = {gallery: [...result1], prints: [...result2]}
-            res.json(art)
-        })
-        .catch(err => {
-            return res.status(400).json({
-                error: "Couldn't get art"
-            })
-        })
+router.get("/refresh", async (req, res) => {
+    console.log("Refreshing");
+    try {
+        const prints = await printModel.find({ deletedAt: null })
+        const gallery = await galleryModel.find({ deletedAt: null })
+        console.log("Refreshed art");
+        res.json({ gallery, prints })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Couldn't get art "})
+    }
 })
 
 router.post("/availability", async (req, res) => {
