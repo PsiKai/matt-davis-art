ab3134eeeb6c40f1cd0681f21e58052ff23f6bb9
david irvin <davidirvin47@gmail.com>
2022-03-24 21:59:06 -0600

    Use soft delete in delete route

--- routes/delete.js
+++ routes/delete.js
@@ -12,18 +12,12 @@ const {Storage} = require("@google-cloud/storage")
 const storage = new Storage({keyFileName: auth})
 
 router.post("/:destination", async (req, res) => {
-    let { img, title, _id } = req.body
+    let { title, _id } = req.body
     const { destination } = req.params
-
-    img = decodeURIComponent(img.split(`${destination}/`)[1])
-
-    const bucket = `matt-d-${destination}`
     const model = require(`../models/${destination}`)
 
     try {
-        const isShared = await hasSharedResource(img, destination)
-        if (!isShared) await storage.bucket(bucket).file(img).delete()
-        await model.deleteOne({_id: _id})
+        await model.updateOne({ _id }, { deletedAt: Date.now() })
         res.json({msg: `${title} was deleted`})
     } catch (error) {
         console.log(error);
